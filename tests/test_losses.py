3cedea8ad4b69828cfc065dbe4f65bc3692fd8d6
Quentin Marcou <quentin.marcou@ap-hm.fr>
2022-02-22 15:48:18 +0100

    Add failing test to reproduce model initialization issue

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -1,10 +1,13 @@
 from unittest import TestCase
+
+import keras
+
 from keras_utils import losses
 from tensorflow.keras.losses import binary_crossentropy
 from tensorflow.keras.losses import Reduction
 from numpy.testing import assert_array_almost_equal
 import numpy as np
-
+import tensorflow as tf
 
 class Test(TestCase):
     def test_weighted_binary_crossentropy(self):
@@ -21,6 +24,12 @@ class Test(TestCase):
         assert_array_almost_equal(np.array([1.83258105, 0.713558]),
                                   wbc(y_true, y_pred, weights,
                                       from_logits=False))
+        # Pass tensorflow tensors as input instead of lists
+        assert_array_almost_equal(np.array([1.83258105, 0.713558]),
+                                  wbc(tf.constant(y_true),
+                                      tf.constant(y_pred),
+                                      tf.constant(weights),
+                                      from_logits=False))
         # Check the wrapper loss class behavior
         wbce = losses.WeightedBinaryCrossentropy(class_weights=weights,
                                                  from_logits=False,
@@ -33,3 +42,11 @@ class Test(TestCase):
                                                  reduction=Reduction.SUM_OVER_BATCH_SIZE)
         assert_array_almost_equal(np.array(1.273069525),
                                   wbce(y_true, y_pred).numpy())
+        # Inside a model pipeline
+        # check that the loss function handles correctly tensors with unknown
+        # dimension (at model initialization batch size is unknown and shape
+        # the first dimension is None and the passed vectors are placeholders
+        model = keras.Sequential([keras.layers.Dense(1)])
+        model.compile(loss=wbce)
+        model.fit(x=[1, 2],
+                  y=y_true)
