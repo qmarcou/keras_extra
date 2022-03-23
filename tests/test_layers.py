b99eeffe02b42b6e683503a623af122950e53be1
Quentin Marcou <quentin.marcou@ap-hm.fr>
2022-03-23 15:35:57 +0100

    Expand ECM test cases

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -13,11 +13,61 @@ class Test(tf.test.TestCase):
                           extremum="min",
                           adjacency_matrix=adj_mat,
                           sparse_adjacency=True)
-        ecm_layer = layers.ExtremumConstraintModule(activation="linear",
-                                                    extremum="min",
-                                                    adjacency_matrix=adj_mat,
-                                                    sparse_adjacency=False)
-        # sample size 1, parent class with smaller val
+        ecm_layer_min = layers.ExtremumConstraintModule(activation="linear",
+                                                        extremum="min",
+                                                        adjacency_matrix=adj_mat,
+                                                        sparse_adjacency=False)
+        ecm_layer_max = layers.ExtremumConstraintModule(activation="linear",
+                                                        extremum="max",
+                                                        adjacency_matrix=adj_mat,
+                                                        sparse_adjacency=False)
+        # sample size 1, 2 class 1 relationship
         input_logits = tf.constant(np.array([[1.0, 2.0]]), dtype=tf.float32)
         self.assertAllEqual(np.array([[1.0, 1.0]]),
-                            ecm_layer(input_logits))
+                            ecm_layer_min(input_logits))
+        self.assertAllEqual(np.array([[1.0, 2.0]]),
+                            ecm_layer_max(input_logits))
+        # Test non linear activation function
+        ecm_layer_min_sigm = layers.ExtremumConstraintModule(activation="sigmoid",
+                                                        extremum="min",
+                                                        adjacency_matrix=adj_mat,
+                                                        sparse_adjacency=False)
+        self.assertAllCloseAccordingToType(
+            tf.keras.activations.sigmoid(np.array([[1.0, 1.0]])),
+            ecm_layer_min_sigm(input_logits))
+        # sample size 2
+        input_logits = tf.constant(np.array([[1.0, 2.0],
+                                             [2.0, 1.0]]), dtype=tf.float32)
+        self.assertAllEqual(np.array([[1.0, 1.0],
+                                      [2.0, 1.0]]),
+                            ecm_layer_min(input_logits))
+        self.assertAllEqual(np.array([[1.0, 2.0],
+                                      [2.0, 2.0]]),
+                            ecm_layer_max(input_logits))
+        # 3 class adjacency matrix, test coherent classification cases
+        # 1 and 2 are child of 0
+        # 3 is child of 1 (hence of 0)
+        adj_mat = np.array([[0, 0, 0, 0], [1, 0, 0, 0],
+                            [1, 0, 0, 0], [1, 1, 0, 0]])
+        input_logits = tf.constant(np.array([[4.0, 3.0, 1.0, 2.0],
+                                             [4.0, 2.0, 1.0, 3.0],
+                                             [1.0, 2.0, 1.0, 3.0]]),
+                                   dtype=tf.float32)
+        ecm_layer_min = layers.ExtremumConstraintModule(activation="linear",
+                                                        extremum="min",
+                                                        adjacency_matrix=adj_mat,
+                                                        sparse_adjacency=False)
+        # transpose adj_mat for max to give a parent->child adj_mat
+        ecm_layer_max = layers.ExtremumConstraintModule(activation="linear",
+                                                        extremum="max",
+                                                        adjacency_matrix=adj_mat
+                                                        .transpose(),
+                                                        sparse_adjacency=False)
+        self.assertAllEqual(np.array([[4.0, 3.0, 1.0, 2.0],
+                                      [4.0, 2.0, 1.0, 2.0],
+                                      [1.0, 1.0, 1.0, 1.0]]),
+                            ecm_layer_min(input_logits))
+        self.assertAllEqual(np.array([[4.0, 3.0, 1.0, 2.0],
+                                      [4.0, 3.0, 1.0, 3.0],
+                                      [3.0, 3.0, 1.0, 3.0]]),
+                            ecm_layer_max(input_logits))
