14d736d28fe587a285a0a9d4e3c2bfaaaed1773c
Quentin Marcou <quentin.marcou@ap-hm.fr>
2022-03-24 19:14:17 +0100

    Add passing tests with keras.Input first layer

--- tests/test_models.py
+++ tests/test_models.py
@@ -9,6 +9,7 @@ class TestSequentialPreOutputLoss(tf.test.TestCase):
     def test___init___(self):
         SequentialPreOutputLoss = models.SequentialPreOutputLoss
         inputL = keras.layers.InputLayer(input_shape=1, name="input")
+        inputT = keras.Input(shape=1)
         outputL = keras.layers.Dense(1, name="output")
         hiddenL = keras.layers.Dense(1, name="hidden")
         SequentialPreOutputLoss()
@@ -23,13 +24,22 @@ class TestSequentialPreOutputLoss(tf.test.TestCase):
                                     loss_layer_name=outputL.name)
         self.assertEqual(1, len(m.outputs))
         m = SequentialPreOutputLoss(layers=[inputL, hiddenL, outputL],
-                                loss_layer_name="hidden")
+                                    loss_layer_name="hidden")
         self.assertEqual(2, len(m.outputs))
         # this should work upon model construction but fail at compile
         # since the loss layer could be added later via self.add before compile
         m = SequentialPreOutputLoss(layers=[inputL, hiddenL, outputL],
                                     loss_layer_name="ZZZ")
         self.assertEqual(1, len(m.outputs))
+        # Test model instantiation with Input tensor instead of InputLayer
+        # SequentialPreOutputLoss(layers=inputT)
+        SequentialPreOutputLoss(layers=[inputT, outputL])
+        m = SequentialPreOutputLoss(layers=[inputT, outputL],
+                                    loss_layer_name="output")
+        m = SequentialPreOutputLoss(layers=[inputT, hiddenL, outputL],
+                                    loss_layer_name="output")
+        m = SequentialPreOutputLoss(layers=[inputT, hiddenL, outputL],
+                                    loss_layer_name="hidden")
 
     def test_compile(self):
         SequentialPreOutputLoss = models.SequentialPreOutputLoss
