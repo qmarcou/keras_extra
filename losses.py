78320c3dc8d626814dcfed149bea9b5efb88fa5d
Quentin Marcou <quentin.marcou@ap-hm.fr>
2022-03-30 16:06:35 +0200

    Split weighted_binary_crossentropy computation

--- losses.py
+++ losses.py
@@ -17,38 +17,8 @@ from tensorflow.python.keras.utils import losses_utils
 def get_ndim(x: tf.Tensor):
     return tf.shape(x).shape[0]
 
-
-def weighted_binary_crossentropy(y_true, y_pred, class_weights,
-                                 from_logits=False):
-    """Computes a weighted version of the binary crossentropy loss.
-
-    Warnings: Out of simplicity I assume binary labels for y_true (0 or 1), and
-    prevent the use of label_smoothing!=0. To fix this I would have to rewrite
-    a more complicated version of the binary_crossentropy backend function with
-    and without logits.
-
-    Standalone usage:
-
-    >>> y_true = [[0, 1], [0, 0]]
-    >>> y_pred = [[0.6, 0.4], [0.4, 0.6]]
-    >>> weights = [[1.0,10.0],[1.0,5.0]]
-    >>> loss = tf.keras.losses.binary_crossentropy(y_true, y_pred, weights)
-    >>> assert loss.shape == (2,)
-    >>> loss.numpy()
-    array([0.916 , 0.714], dtype=float32)
-
-    Args:
-      y_true: Ground truth values. shape = `[batch_size, d0, .. dN]`.
-      y_pred: The predicted values. shape = `[batch_size, d0, .. dN]`.
-      class_weights: The weights to apply depending on the value of y_true.
-       shape = `[d0, .. dN, 2]`
-      from_logits: Whether `y_pred` is expected to be a logits tensor. By default,
-        we assume that `y_pred` encodes a probability distribution.
-
-    Returns:
-      Weighted binary crossentropy loss value.
-      shape = `[batch_size, d0, .. dN-1]`.
-    """
+def weighted_binary_crossentropy_per_node(y_true, y_pred, class_weights,
+                                          from_logits=False):
     y_pred = ops.convert_to_tensor_v2_with_dispatch(y_pred)
     y_true = tf.cast(y_true, y_pred.dtype)
     neg_y_true = tf.ones_like(y_true) - y_true
@@ -107,7 +77,46 @@ def weighted_binary_crossentropy(y_true, y_pred, class_weights,
                  size=slice_size),
         shape=tf.shape(y_true)),
         y_true)
+    return weighted_cross_ent
 
+def weighted_binary_crossentropy(y_true, y_pred, class_weights,
+                                 from_logits=False):
+    """Computes a weighted version of the binary crossentropy loss.
+
+    Warnings: Out of simplicity I assume binary labels for y_true (0 or 1), and
+    prevent the use of label_smoothing!=0. To fix this I would have to rewrite
+    a more complicated version of the binary_crossentropy backend function with
+    and without logits.
+
+    Standalone usage:
+
+    >>> y_true = [[0, 1], [0, 0]]
+    >>> y_pred = [[0.6, 0.4], [0.4, 0.6]]
+    >>> weights = [[1.0,10.0],[1.0,5.0]]
+    >>> loss = tf.keras.losses.binary_crossentropy(y_true, y_pred, weights)
+    >>> assert loss.shape == (2,)
+    >>> loss.numpy()
+    array([0.916 , 0.714], dtype=float32)
+
+    Args:
+      y_true: Ground truth values. shape = `[batch_size, d0, .. dN]`.
+      y_pred: The predicted values. shape = `[batch_size, d0, .. dN]`.
+      class_weights: The weights to apply depending on the value of y_true.
+       shape = `[d0, .. dN, 2]`
+      from_logits: Whether `y_pred` is expected to be a logits tensor. By default,
+        we assume that `y_pred` encodes a probability distribution.
+
+    Returns:
+      Weighted binary crossentropy loss value.
+      shape = `[batch_size, d0, .. dN-1]`.
+    """
+
+    weighted_cross_ent = weighted_binary_crossentropy_per_node(
+        y_true=y_true,
+        y_pred=y_pred,
+        class_weights=class_weights,
+        from_logits=from_logits
+    )
     return K.mean(weighted_cross_ent, axis=-1)
 
 
