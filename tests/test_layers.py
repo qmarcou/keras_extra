6fe530c9ffa319ad614775c6e25a4348b9b515fd
Quentin Marcou <quentin.marcou@ap-hm.fr>
2022-04-07 16:44:05 +0200

    Update ECM layer tests to cope with negative logits

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -22,10 +22,10 @@ class Test(tf.test.TestCase):
                                                         adjacency_matrix=adj_mat,
                                                         sparse_adjacency=False)
         # sample size 1, 2 class 1 relationship
-        input_logits = tf.constant(np.array([[1.0, 2.0]]), dtype=tf.float32)
-        self.assertAllEqual(np.array([[1.0, 1.0]]),
+        input_logits = tf.constant(np.array([[-1.0, 2.0]]), dtype=tf.float32)
+        self.assertAllEqual(np.array([[-1.0, -1.0]]),
                             ecm_layer_min(input_logits))
-        self.assertAllEqual(np.array([[1.0, 2.0]]),
+        self.assertAllEqual(np.array([[-1.0, 2.0]]),
                             ecm_layer_max(input_logits))
         # Test non linear activation function
         ecm_layer_min_sigm = layers.ExtremumConstraintModule(activation="sigmoid",
@@ -33,15 +33,15 @@ class Test(tf.test.TestCase):
                                                         adjacency_matrix=adj_mat,
                                                         sparse_adjacency=False)
         self.assertAllCloseAccordingToType(
-            tf.keras.activations.sigmoid(np.array([[1.0, 1.0]])),
+            tf.keras.activations.sigmoid(np.array([[-1.0, -1.0]])),
             ecm_layer_min_sigm(input_logits))
         # sample size 2
-        input_logits = tf.constant(np.array([[1.0, 2.0],
-                                             [2.0, 1.0]]), dtype=tf.float32)
-        self.assertAllEqual(np.array([[1.0, 1.0],
-                                      [2.0, 1.0]]),
+        input_logits = tf.constant(np.array([[-1.0, 2.0],
+                                             [2.0, -1.0]]), dtype=tf.float32)
+        self.assertAllEqual(np.array([[-1.0, -1.0],
+                                      [2.0, -1.0]]),
                             ecm_layer_min(input_logits))
-        self.assertAllEqual(np.array([[1.0, 2.0],
+        self.assertAllEqual(np.array([[-1.0, 2.0],
                                       [2.0, 2.0]]),
                             ecm_layer_max(input_logits))
         # 3 class adjacency matrix, test coherent classification cases
@@ -49,9 +49,9 @@ class Test(tf.test.TestCase):
         # 3 is child of 1 (hence of 0)
         adj_mat = np.array([[0, 0, 0, 0], [1, 0, 0, 0],
                             [1, 0, 0, 0], [1, 1, 0, 0]])
-        input_logits = tf.constant(np.array([[4.0, 3.0, 1.0, 2.0],
-                                             [4.0, 2.0, 1.0, 3.0],
-                                             [1.0, 2.0, 1.0, 3.0]]),
+        input_logits = tf.constant(np.array([[4.0, 3.0, -1.0, 2.0],
+                                             [4.0, 2.0, -1.0, 3.0],
+                                             [-1.0, 2.0, -1.0, 3.0]]),
                                    dtype=tf.float32)
         ecm_layer_min = layers.ExtremumConstraintModule(activation="linear",
                                                         extremum="min",
@@ -63,13 +63,13 @@ class Test(tf.test.TestCase):
                                                         adjacency_matrix=adj_mat
                                                         .transpose(),
                                                         sparse_adjacency=False)
-        self.assertAllEqual(np.array([[4.0, 3.0, 1.0, 2.0],
-                                      [4.0, 2.0, 1.0, 2.0],
-                                      [1.0, 1.0, 1.0, 1.0]]),
+        self.assertAllEqual(np.array([[4.0, 3.0, -1.0, 2.0],
+                                      [4.0, 2.0, -1.0, 2.0],
+                                      [-1.0, -1.0, -1.0, -1.0]]),
                             ecm_layer_min(input_logits))
-        self.assertAllEqual(np.array([[4.0, 3.0, 1.0, 2.0],
-                                      [4.0, 3.0, 1.0, 3.0],
-                                      [3.0, 3.0, 1.0, 3.0]]),
+        self.assertAllEqual(np.array([[4.0, 3.0, -1.0, 2.0],
+                                      [4.0, 3.0, -1.0, 3.0],
+                                      [3.0, 3.0, -1.0, 3.0]]),
                             ecm_layer_max(input_logits))
 
         # Test building with partially unknown input_shape
