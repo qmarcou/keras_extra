ef5121c577ebac6c41bdd71693353ab49215404c
Quentin Marcou <quentin.marcou@ap-hm.fr>
2022-04-29 17:31:52 +0200

    Implement nanpercentile, test that it yields the same results as percentile when no NaNs are provided.

--- stats.py
+++ stats.py
@@ -2,9 +2,17 @@
 import tensorflow as tf
 import tensorflow_probability as tfp
 import numpy as np
+from keras_utils import utils
 
 
-def nanpercentile(x, q, **kwargs) -> tf.Tensor:
+def nanpercentile(x,
+                  q,
+                  axis=None,
+                  interpolation=None,
+                  keepdims=False,
+                  validate_args=False,
+                  preserve_gradients=True,
+                  name=None) -> tf.Tensor:
     """
     Compute percentile over all axes of a Tensor ignoring NaNs.
     Parameters
@@ -19,9 +27,85 @@ def nanpercentile(x, q, **kwargs) -> tf.Tensor:
     -------
     A rank(1) Tensor of length q.
     """
-    if kwargs.get("axis") is not None:
-        raise ValueError("nanpercentile does not support the axis argument.")
-    mask = tf.logical_not(tf.math.is_nan(x))
-    # use a boolean mask to remove nans and flatten the Tensor if necessary
-    masked_x = tf.boolean_mask(mask=mask, tensor=x, axis=None)
-    return tfp.stats.percentile(masked_x, q, **kwargs)
+    if keepdims:
+        raise NotImplementedError("keepdims=True option is not implemented.")
+
+    if axis is None or (tf.rank(x) == 1 and axis == 0):
+        # If axis is None simply flatten the x Tensor and compute percentile
+        # over the flattened version
+        mask = tf.logical_not(tf.math.is_nan(x))
+        # use a boolean mask to remove nans and flatten the Tensor if necessary
+        masked_x = tf.boolean_mask(mask=mask, tensor=x, axis=None)
+        return tfp.stats.percentile(x=masked_x, q=q, axis=axis,
+                                    interpolation=interpolation,
+                                    keepdims=False,
+                                    validate_args=validate_args,
+                                    preserve_gradients=preserve_gradients,
+                                    name=name)
+    else:
+        # Check and process the axis argument
+        if not isinstance(axis, tf.Tensor):
+            axis = tf.constant(axis, dtype=tf.int32)
+        if tf.rank(axis) > 1:
+            raise ValueError(
+                "Expected a 0 or 1D tensor like object for axis, got"
+                " rank." + str(tf.rank(axis).numpy()))
+        else:
+            axis = tf.reshape(tensor=axis, shape=(-1,))
+
+        # Send the collapsed axes to last dimensions
+        x = utils.move_axis_to_last_dim(x=x, axis=axis)
+
+        # Collapse all other dimensions to a vector that can be iterated on
+        # the final shape is (-1, length_of_flattened_axis_of_interest)
+        init_shape = tf.shape(input=x)
+        axis_len = tf.shape(axis)[0]
+        axis_collapsed_len = tf.math.cumprod(
+            init_shape[-axis_len:]  # axes of interest are last
+        )[-1]
+        x = tf.reshape(tensor=x, shape=(-1, axis_collapsed_len))
+
+        # Now check nan values and create a ragged Tensor from the boolean mask
+        mask = tf.logical_not(tf.math.is_nan(x))
+        ragged_x = tf.ragged.boolean_mask(data=x, mask=mask, name='drop_NaNs')
+
+        # Finally apply the percentile function to each row of this ragged
+        # Tensor
+        q_rank = tf.rank(q)
+        if tf.equal(q_rank, 0):
+            # Expected shape of the Tensor returned by tfp.stats.percentile
+            expect_shape = ()
+        else:
+            expect_shape = (None,)
+        res = tf.map_fn(
+            fn=lambda t: tfp.stats.percentile(
+                x=t, q=q, axis=None,
+                interpolation=interpolation,
+                keepdims=False,
+                validate_args=validate_args,
+                preserve_gradients=preserve_gradients,
+                name=name),
+            elems=ragged_x,
+            # enforce return of tf.Tensor instead if tf.RaggedTensor
+            fn_output_signature=tf.TensorSpec(shape=expect_shape),
+            name="compute_percentile"
+        )
+
+        # Reshape vector or 2D result to the correct shape
+        restored_dims = init_shape[0:-axis_len]
+        if tf.equal(q_rank, 0):
+            return tf.reshape(tensor=res, shape=restored_dims)
+        elif tf.equal(q_rank, 1):
+            res = tf.reshape(tensor=res,
+                             shape=tf.concat(
+                                 [restored_dims,
+                                  tf.slice(tf.shape(q), begin=(0,), size=(1,))],
+                                 axis=0))
+            # Now move the last axis for different percentiles to the first
+            # axis the same way it would have been returned by
+            # tfp.stats.percentile
+            return utils.move_axis_to_first_dim(x=res, axis=-1)
+        else:
+            # Should never be reached since tfp.stats.percentile should have
+            # thrown an exception already
+            raise ValueError("q must be a 0 or 1D Tensor or alike.")
--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,18 +1,39 @@
 from unittest import TestCase
 from keras_utils import stats
 import tensorflow as tf
+import tensorflow_probability as tfp
 import numpy as np
 
 
 class Test_NanPercentile(tf.test.TestCase):
     def test_nanpercentile(self):
-        x = tf.constant([1, 2, 3, 4, 5, 6, 7, 8, 9],dtype=tf.float32)
+        x = tf.constant([1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=tf.float32)
         self.assertEqual(5, stats.nanpercentile(x, 50))
-        self.assertRaises(ValueError,
-                          stats.nanpercentile, x, 50, axis=0)
+        self.assertEqual(5, stats.nanpercentile(x, 50, axis=0))
+
         x_nan = tf.concat([tf.fill(value=np.nan, dims=(4,)),
                            x,
                            tf.fill(value=np.nan, dims=(4,))], axis=0)
         p = [10.0, 10.5, 25.0, 50.0, 75.0]
         self.assertAllEqual(stats.nanpercentile(x, p),
                             stats.nanpercentile(x_nan, p))
+
+        rand_x = tf.random.uniform(shape=(2, 3, 4, 5))
+        # Check that without NaNs the function returns the same results
+        # as tfp.stats.percentile
+        self.assertAllEqual(
+            tfp.stats.percentile(x=rand_x, q=50, axis=-1),
+            stats.nanpercentile(x=rand_x, q=50, axis=-1)
+        )
+        self.assertAllEqual(
+            tfp.stats.percentile(x=rand_x, q=[50], axis=-1),
+            stats.nanpercentile(x=rand_x, q=[50], axis=-1)
+        )
+        self.assertAllEqual(
+            tfp.stats.percentile(x=rand_x, q=[50, 75], axis=-1),
+            stats.nanpercentile(x=rand_x, q=[50, 75], axis=-1)
+        )
+        self.assertAllEqual(
+            tfp.stats.percentile(x=rand_x, q=[50, 75], axis=2),
+            stats.nanpercentile(x=rand_x, q=[50, 75], axis=2)
+        )
