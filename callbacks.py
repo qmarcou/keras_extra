74a19ac5ec05453abbd738d8b21500c0405472a6
Quentin Marcou <quentin.marcou@ap-hm.fr>
2022-06-10 16:22:02 +0200

    Draft a data evluation callback relying on the access to logs dictionnaries from the fit method

--- callbacks.py
+++ callbacks.py
@@ -1,5 +1,6 @@
 """A collection of custom Keras callbacks."""
 from tensorflow import keras
+import tensorflow as tf
 
 
 class BatchLossLRHistory(keras.callbacks.Callback):
@@ -14,3 +15,43 @@ class BatchLossLRHistory(keras.callbacks.Callback):
         self.losses.append(logs.get('loss'))
         self.learning_rates.append(
             self.model.optimizer.lr(self.model.optimizer.iterations))
+
+
+class DataEvaluator(keras.callbacks.Callback):
+    """
+    A callback to evaluate a model on specific data.
+
+    /!\ This Callback will function only for Models exposing epoch_logs and
+    training_logs class attributes.
+    """
+
+    def __init__(self, x, y,
+                 eval_prefix="dataEval_", **kwargs):
+        self.x_data = x
+        self.y_data = y
+        self.eval_prefix = eval_prefix
+        self.eval_kwargs = kwargs
+        if kwargs.get("return_dict") is not None:
+            raise AttributeError("return_dict cannot be specified when used "
+                                 "in the DataEvaluator callback")
+
+    def on_epoch_end(self, epoch, logs=None):
+        # Evaluate the model on the provided data
+        eval_output = self.model.evaluate(
+            x=self.x_data,
+            y=self.y_data,
+            return_dict=True,
+            **self.eval_kwargs
+        )
+        # Add the prefix to all keys of the dict
+        eval_output = {"dev_" + str(key): val for key, val in
+                       eval_output.items()}
+
+        # Check that none of the new keys overshadow the previous ones
+        if len(set(eval_output.keys())
+                       .intersection(self.model.epoch_logs.keys())) > 0:
+            raise RuntimeError("Names resulting from the DataEvaluator "
+                               "overshadows names from the logs dict, "
+                               "pick a different eval_prefix")
+        # Update epoch_logs dict
+        self.model.epoch_logs.update(eval_output)
