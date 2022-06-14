32ce81beb754791b38b012515b9eb2bf46b63887
Quentin Marcou <quentin.marcou@ap-hm.fr>
2022-06-14 12:05:46 +0200

    Fix DataEvaluator test

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -10,13 +10,16 @@ class TestDataEvaluator(TestCase):
         y_train = [0, 0, 1, 1]
         model = keras.Sequential(keras.layers.Dense(1, activation="sigmoid"))
         model.compile(loss="binary_crossentropy", metrics="accuracy")
-        data_eval = callbacks.DataEvaluator(x=x_train, y=y_train,
-                                            eval_prefix="dataEvalTester_")
         x_test = [[3], [2], [1], [0]]
         y_test = [0, 0, 1, 1]
-        hist: keras.callbacks.History = model.fit(x=x_test,
-                                                  y=y_test,
+        data_eval = callbacks.DataEvaluator(x=x_test, y=y_test,
+                                            eval_prefix="dataEvalTester_")
+        hist: keras.callbacks.History = model.fit(x=x_train,
+                                                  y=y_train,
                                                   epochs=3,
-                                                  callbacks=[data_eval],)
-
-        print(hist.history)
+                                                  callbacks=[data_eval],
+                                                  verbose=False)
+        eval = model.evaluate(x=x_test,
+                              y=y_test, return_dict=True)
+        self.assertAlmostEqual(hist.history['dataEvalTester_loss'][-1],
+                               eval['loss'])
