e1143b71956df8230673f57ff9616ac967fc256b
Quentin Marcou <quentin.marcou@ap-hm.fr>
2022-09-19 17:58:41 +0200

    Add ability to retry a previously erroneous trial in the OOM wrapper script

--- errors.py
+++ errors.py
@@ -5,3 +5,10 @@ class KerasTunerSearchOOM(errors.ResourceExhaustedError):
     """A specific Exception to be thrown when Keras Tuner is out of memory
     due to Tensorflow's infamous memory leak """
     pass
+
+
+class KerasTunerSearchOOMSameTrial(errors.ResourceExhaustedError):
+    """Another specific Exception to be thrown when Keras Tuner is out of memory
+    due to Tensorflow's infamous memory leak, and it happens on a previously
+    erroneous trial."""
+    pass
