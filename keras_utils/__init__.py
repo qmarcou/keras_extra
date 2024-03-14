b54f0a88f0498f8fff3fe0ad30e2691f87e4568a
q.marcou <18257721+qmarcou@users.noreply.github.com>
2024-03-14 15:34:53 +0100

    Clean up unnecessary path fiddling in __init__ files

--- keras_utils/__init__.py
+++ keras_utils/__init__.py
@@ -4,11 +4,3 @@ from . import losses
 from . import metrics
 from . import models
 from . import schedules
-
-import sys
-from pathlib import Path
-import os
-
-libdir = os.path.dirname(__file__)
-srcdir = libdir+'/..'
-sys.path.insert(0, str(Path(os.path.abspath(srcdir))))
